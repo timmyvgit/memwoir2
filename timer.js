6a780b460dd8f9f3624828e44eb6a20c94f191b2
Tim Vuong <74928150+timmyvgit@users.noreply.github.com>
2022-09-16 11:55:02 -0500

    update

---
+++ timer.js
@@ -0,0 +1,11 @@
+const startTime = 10;
+let timeRemain = startTime * 60;
+
+const countDown = document.getElementById('countDown');
+
+function updateCountDown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML =tim--
+}
